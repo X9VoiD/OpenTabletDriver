266f12ceafacf8207588f57a064f878e70dee65b
X9VoiD <oscar.silvestrexx@gmail.com>
2021-01-01 02:31:26 +0800

    Code cleanup

--- OpenTabletDriver.UX.Wpf/Program.cs
+++ OpenTabletDriver.UX.Wpf/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Eto.Forms;
 
 namespace OpenTabletDriver.UX.Wpf
 {
@@ -7,8 +10,36 @@ namespace OpenTabletDriver.UX.Wpf
         [STAThread]
         public static void Main(string[] args)
         {
-            _ = new SingleInstance();
+            SingleInstance();
             App.Run(Eto.Platforms.Wpf, args);
+        }
+
+        private static void SingleInstance()
+        {
+            var handle = new EventWaitHandle(false, EventResetMode.AutoReset, "OpenTabletDriver.UX.Wpf", out var host);
+            if (host)
+            {
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        handle.WaitOne();
+                        Application.Instance?.AsyncInvoke(() =>
+                        {
+                            var form = Application.Instance?.MainForm;
+                            form?.Show();
+                            form.WindowState = WindowState.Normal;
+                            form.BringToFront();
+                            form.WindowStyle = WindowStyle.Default;
+                        });
+                    }
+                });
+            }
+            else
+            {
+                handle.Set();
+                Environment.Exit(0);
+            }
         }
     }
 }
