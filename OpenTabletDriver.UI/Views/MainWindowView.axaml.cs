fd6464dabe5f974bf2e193b09915ce48b0e77cfd
X9VoiD <oscar.silvestrexx@gmail.com>
2023-03-15 02:40:30 +0800

    a

--- OpenTabletDriver.UI/Views/MainWindowView.axaml.cs
+++ OpenTabletDriver.UI/Views/MainWindowView.axaml.cs
@@ -21,7 +21,6 @@ public partial class MainWindowView : Window
         }
 
         // Force acrylic blur for everything else
-        // TODO: should be possible to disable
         TransparencyLevelHint = WindowTransparencyLevel.AcrylicBlur;
     }
 }
